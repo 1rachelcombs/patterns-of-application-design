fab9d1a3cf3245cb88b2996895afbccab6ab2e72
1rachelcombs <70183004+1rachelcombs@users.noreply.github.com>
2020-08-30 20:43:24 -0500

    Add files via upload
    Lesson 1 - Playground 3

---
+++ Strings.playground/Pages/Exercise-Making a List.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+/*:
+ ## Exercise: Making a List
+ 
+ Lists are great. Here are some constants describing some of the things you’ve learned about strings so far:
+ */
+let constants = "Declaring string constants"
+let unicode = "Unicode characters (😎)"
+let combining = "Combining strings using +"
+let interpolation = "String interpolation (aka Fill in the Blanks)"
+let escaping = "Escape characters for \"special powers\""
+let newline = "Making new lines"
+
+
+
+
+let makelist = "I learned: \n\(constants); \n\(unicode); \n\(combining); \n\(interpolation); \n\(escaping); \n\(newline)."
+
+//:
+//:[Previous](@previous)  |  page 13 of 16  |  [Next: Exercise: A Restaurant](@next)
