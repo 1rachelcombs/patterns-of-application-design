a7c532a8e1799d037c7bf67aa137b26d62009c97
1rachelcombs <70183004+1rachelcombs@users.noreply.github.com>
2020-08-30 21:05:14 -0500

    Add files via upload

---
+++ Assisnment 1.playground/Contents.swift
@@ -0,0 +1,12 @@
+var name = "Rachel Combs"
+var age = 25
+var birthplace = "Benton, AR"
+var currentlyLive = "Jonesboro,AR"
+var favoriteColor = "purple"
+var favoriteAnimal = "a dog"
+var aboutME = "dog mom, photographer, graphic designer"
+var career = "I currently work as a Marketing Coordinator but I would love to be a Marketing Manager some day"
+
+var bio = "Hi, my name is \(name). I am \(age) years old. I was born in \(birthplace) but I currently live here in \(currentlyLive). My favorite color is \(favoriteColor). My favorite animal is \(favoriteAnimal). I am a \(aboutME). \(career)."
+
+var bio2 = "Hi, my name is \(name). \n I am \(age) years old. \n I was born in \(birthplace) but I currently live here in \(currentlyLive). \n My favorite color is \(favoriteColor). \n My favorite animal is \(favoriteAnimal). \n I am a \(aboutME). \n \(career)."
