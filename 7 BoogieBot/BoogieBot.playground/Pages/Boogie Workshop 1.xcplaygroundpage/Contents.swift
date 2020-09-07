6c14712afaa8b87506ccb8b5f2bd44d4ccc114f4
1rachelcombs <70183004+1rachelcombs@users.noreply.github.com>
2020-09-06 20:17:40 -0500

    Add files via upload

---
+++ 7 BoogieBot/BoogieBot.playground/Pages/Boogie Workshop 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+/*:
+ ## Boogie Workshop
+ 
+ This page is here for you to create your own routines. 
+ 
+ Remember the moves:
+ 
+ - `leftArmUp()`, `leftArmDown()`, `rightArmUp()`, `rightArmDown()`
+ - `leftLegUp()`, `leftLegDown()`, `rightLegUp()`, `rightLegDown()`
+ - `shakeItLeft()`, `shakeItRight()`, `shakeItCenter()`
+ - `jumpUp()`, `jumpDown()`
+ - `fabulize()`, `defabulize()`
+ 
+ To sign your work:
+ 
+ `setBotTitle("My Awesome Dance")`\
+ `setBotSubtitle("By The Boogiemaster")`
+*/
+startBot()
+startRecording()
+
+fabulize()
+fabulize()
+fabulize()
+fabulize()
+rightArmUp()
+leftArmUp()
+rightArmDown()
+leftArmDown()
+rightArmUp()
+leftArmUp()
+rightLegUp()
+leftLegUp()
+rightLegDown()
+leftLegDown()
+jumpUp()
+jumpDown()
+
+setBotTitle("My Awesome Dance")
+setBotSubtitle("By The Boogiemaster")
+
+//: Continue with your creativity.
+//:
+//:[Previous](@previous)  |  page 10 of 13  |  [Next: Boogie Workshop](@next)
---
+++ 7 BoogieBot/BoogieBot.playground/Pages/Boogie Workshop 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+/*:
+ ## Boogie Workshop
+ 
+ This page is here for you to create your own routines.
+ 
+ Remember the moves:
+ 
+ - `leftArmUp()`, `leftArmDown()`, `rightArmUp()`, `rightArmDown()`
+ - `leftLegUp()`, `leftLegDown()`, `rightLegUp()`, `rightLegDown()`
+ - `shakeItLeft()`, `shakeItRight()`, `shakeItCenter()`
+ - `jumpUp()`, `jumpDown()`
+ - `fabulize()`, `defabulize()`
+ 
+ To sign your work:
+ 
+ `setBotTitle("My Awesome Dance")`\
+ `setBotSubtitle("By The Boogiemaster")`
+ */
+startBot()
+startRecording()
+
+fabulize()
+defabulize()
+fabulize()
+defabulize()
+fabulize()
+jumpUp()
+jumpDown()
+shakeItLeft()
+shakeItRight()
+shakeItCenter()
+rightArmUp()
+rightArmDown()
+defabulize()
+
+
+
+setBotTitle("My Awesome Dance")
+setBotSubtitle("By The Boogiemaster")
+
+
+//: Continue with your creativity.
+//:
+//:[Previous](@previous)  |  page 11 of 13  |  [Next: Boogie Workshop](@next)
